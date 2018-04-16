bb34949aa913dbab347c310447bf33dcf72d0454
Jeremy Soller <jeremy@system76.com>
2018-04-16 12:52:48 -0600

    Break into two packages, add simple cli

---
+++ src/lib.rs
@@ -0,0 +1,168 @@
+extern crate buildchain;
+extern crate ecflash;
+extern crate libc;
+extern crate lzma;
+extern crate plain;
+extern crate serde_json;
+extern crate sha2;
+extern crate tar;
+extern crate tempdir;
+extern crate uuid;
+
+use buildchain::{Downloader, Manifest};
+use std::fs;
+use std::path::Path;
+
+mod bios;
+mod boot;
+mod config;
+mod download;
+mod ec;
+mod me;
+mod mount;
+mod util;
+
+pub use bios::bios;
+pub use ec::ec;
+pub use me::me;
+
+// Helper function for errors
+pub fn err_str<E: ::std::fmt::Display>(err: E) -> String {
+    format!("{}", err)
+}
+
+pub fn firmware_id() -> Result<String, String> {
+    let (bios_model, _bios_version) = bios::bios()?;
+    let (ec_project, _ec_version) = ec::ec_or_none(true);
+    let ec_hash = util::sha256(ec_project.as_bytes());
+    Ok(format!("{}_{}", bios_model, ec_hash))
+}
+
+fn remove_dir<P: AsRef<Path>>(path: P) -> Result<(), String> {
+    if path.as_ref().exists() {
+        eprintln!("removing {}", path.as_ref().display());
+        match fs::remove_dir_all(&path) {
+            Ok(()) => (),
+            Err(err) => {
+                return Err(format!("failed to remove {}: {}", path.as_ref().display(), err));
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub fn download() -> Result<(String, String), String> {
+    let firmware_id = firmware_id()?;
+
+    let dl = Downloader::new(
+        config::KEY,
+        config::URL,
+        config::PROJECT,
+        config::BRANCH,
+        Some(config::CERT)
+    )?;
+
+    let tail = dl.tail()?;
+
+    let cache = download::Cache::new(config::CACHE, Some(dl))?;
+
+    eprintln!("downloading manifest.json");
+    let manifest_json = cache.object(&tail.digest)?;
+    let manifest = serde_json::from_slice::<Manifest>(&manifest_json).map_err(|e| e.to_string())?;
+
+    let _updater_data = {
+        let file = "system76-firmware-update.tar.xz";
+        eprintln!("downloading {}", file);
+        let digest = manifest.files.get(file).ok_or(format!("{} not found", file))?;
+        cache.object(&digest)?
+    };
+
+    let firmware_data = {
+        let file = format!("{}.tar.xz", firmware_id);
+        eprintln!("downloading {}", file);
+        let digest = manifest.files.get(&file).ok_or(format!("{} not found", file))?;
+        cache.object(&digest)?
+    };
+
+    let changelog = util::extract_file(&firmware_data, "./changelog.json").map_err(err_str)?;
+
+    Ok((tail.digest.to_string(), changelog))
+}
+
+fn extract<P: AsRef<Path>>(digest: &str, file: &str, path: P) -> Result<(), String> {
+    let cache = download::Cache::new(config::CACHE, None)?;
+
+    let manifest_json = cache.object(&digest)?;
+    let manifest = serde_json::from_slice::<Manifest>(&manifest_json).map_err(|e| e.to_string())?;
+
+    let data = {
+        let digest = manifest.files.get(file).ok_or(format!("{} not found", file))?;
+        cache.object(&digest)?
+    };
+
+    eprintln!("extracting {} to {}", file, path.as_ref().display());
+    match util::extract(&data, &path) {
+        Ok(()) => (),
+        Err(err) => {
+            return Err(format!("failed to extract {} to {}: {}", file, path.as_ref().display(), err));
+        }
+    }
+
+    Ok(())
+}
+
+pub fn schedule(digest: &str) -> Result<(), String> {
+    let firmware_id = firmware_id()?;
+
+    if ! Path::new("/sys/firmware/efi").exists() {
+        return Err(format!("must be run using UEFI boot"));
+    }
+
+    let updater_file = "system76-firmware-update.tar.xz";
+    let firmware_file = format!("{}.tar.xz", firmware_id);
+    let updater_dir = Path::new("/boot/efi/system76-firmware-update");
+
+    boot::unset_next_boot()?;
+
+    remove_dir(&updater_dir)?;
+
+    let updater_tmp = match tempdir::TempDir::new_in("/boot/efi", "system76-firmware-update") {
+        Ok(ok) => ok,
+        Err(err) => {
+            return Err(format!("failed to create temporary directory: {}", err));
+        }
+    };
+
+    extract(digest, updater_file, updater_tmp.path())?;
+
+    extract(digest, &firmware_file, &updater_tmp.path().join("firmware"))?;
+
+    let updater_tmp_dir = updater_tmp.into_path();
+    eprintln!("moving {} to {}", updater_tmp_dir.display(), updater_dir.display());
+    match fs::rename(&updater_tmp_dir, &updater_dir) {
+        Ok(()) => (),
+        Err(err) => {
+            let _ = remove_dir(&updater_tmp_dir);
+            return Err(format!("failed to move {} to {}: {}", updater_tmp_dir.display(), updater_dir.display(), err));
+        }
+    }
+
+    boot::set_next_boot()?;
+
+    eprintln!("Firmware update scheduled. Reboot your machine to install.");
+
+    Ok(())
+}
+
+pub fn unschedule() -> Result<(), String> {
+    let updater_dir = Path::new("/boot/efi/system76-firmware-update");
+
+    boot::unset_next_boot()?;
+
+    remove_dir(&updater_dir)?;
+
+    eprintln!("Firmware update cancelled.");
+
+    Ok(())
+}
