afb69442a37441bcecfa6de3a6609ff6146b7d0d
Jeremy Soller <jeremy@system76.com>
2020-10-02 10:14:38 -0600

    1.0.19: - Update ectool to ensure locked EC access - Test that DMI vendor contains System76

--- src/ec.rs
+++ src/ec.rs
@@ -1,7 +1,7 @@
 use ecflash::{Ec, EcFlash};
-use ectool::Timeout;
+use ectool::{Access, AccessLpcLinux};
 use std::str;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
 use crate::err_str;
 
@@ -10,42 +10,19 @@ pub fn ectool_err<E: ::std::fmt::Debug>(err: E) -> String {
     format!("{:?}", err)
 }
 
-pub struct StdTimeout {
-    instant: Instant,
-    duration: Duration,
-}
-
-impl StdTimeout {
-    pub fn new(duration: Duration) -> Self {
-        StdTimeout {
-            instant: Instant::now(),
-            duration
-        }
-    }
-}
-
-impl Timeout for StdTimeout {
-    fn reset(&mut self) {
-        self.instant = Instant::now();
-    }
-
-    fn running(&self) -> bool {
-        self.instant.elapsed() < self.duration
-    }
-}
-
 pub fn ec(primary: bool) -> Result<(String, String), String> {
     if primary {
         unsafe {
-            if let Ok(mut ec) = ectool::Ec::new(
-                StdTimeout::new(Duration::new(1, 0))
-            ) {
-                let version = {
-                    let mut data = [0; 256];
-                    let count = ec.version(&mut data).map_err(ectool_err)?;
-                    str::from_utf8(&data[..count]).map_err(err_str)?.to_string()
-                };
-                return Ok(("76ec".to_string(), version));
+            if let Ok(access) = AccessLpcLinux::new(Duration::new(1, 0)) {
+                let data_size = access.data_size();
+                if let Ok(mut ec) = ectool::Ec::new(access) {
+                    let version = {
+                        let mut data = vec![0; data_size];
+                        let count = ec.version(&mut data).map_err(ectool_err)?;
+                        str::from_utf8(&data[..count]).map_err(err_str)?.to_string()
+                    };
+                    return Ok(("76ec".to_string(), version));
+                }
             }
         }
     }
