7f66e87e82270ab388db9246195fed7c147d94b2
Jeremy Soller <jeremy@system76.com>
2021-02-02 09:21:47 -0700

    1.0.22 - Add thelio-major-b3

--- src/lib.rs
+++ src/lib.rs
@@ -78,6 +78,7 @@ const MODEL_WHITELIST: &[&str] = &[
     "thelio-major-b1",
     "thelio-major-b1.1",
     "thelio-major-b2",
+    "thelio-major-b3",
     "thelio-major-r1",
     "thelio-major-r2",
     "thelio-major-r2.1",
