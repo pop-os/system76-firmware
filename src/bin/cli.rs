356668a8243e3a0546239ebd3f36031f7a5a71d6
Ian Douglas Scott <idscott@system76.com>
2022-01-03 12:59:38 -0800

    chore: Update to clap 3

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,4 +1,4 @@
-use clap::{App, AppSettings, Arg, SubCommand};
+use clap::{App, AppSettings, Arg};
 use std::{io, process};
 use system76_firmware::*;
 
@@ -23,25 +23,23 @@ fn tool() -> Result<(), String> {
     let matches = App::new("system76-firmware-cli")
         .about("Download and install updates of System76 firmware")
         .setting(AppSettings::SubcommandRequired)
-        .setting(AppSettings::DisableVersion)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(SubCommand::with_name("schedule")
+        .subcommand(App::new("schedule")
                     .about("Schedule installation of firmware for next boot")
-                    .arg(Arg::with_name("open")
+                    .arg(Arg::new("open")
                          .help("Schedule install of open firmware")
                          .long("open"))
-                    .arg(Arg::with_name("proprietary")
+                    .arg(Arg::new("proprietary")
                          .help("Schedule install of proprietary firmware")
                          .long("proprietary")
                          .conflicts_with("open")))
-        .subcommand(SubCommand::with_name("unschedule")
+        .subcommand(App::new("unschedule")
                     .about("Cancel scheduled firmware installation"))
-        .subcommand(SubCommand::with_name("thelio-io")
+        .subcommand(App::new("thelio-io")
                     .about("Update Thelio IO firmware"))
         .get_matches();
 
     match matches.subcommand() {
-        ("schedule", Some(sub_m)) => {
+        Some(("schedule", sub_m)) => {
             let transition_kind = if sub_m.is_present("open") {
                 TransitionKind::Open
             } else if sub_m.is_present("proprietary") {
@@ -60,13 +58,13 @@ fn tool() -> Result<(), String> {
                 Err(err) => Err(format!("failed to schedule: {}", err))
             }
         }
-        ("unschedule", Some(_)) => {
+        Some(("unschedule", _)) => {
             match unschedule(&efi_dir) {
                 Ok(()) => Ok(()),
                 Err(err) => Err(format!("failed to unschedule: {}", err))
             }
         }
-        ("thelio-io", Some(_)) => {
+        Some(("thelio-io", _)) => {
             let (digest, _revision) = match thelio_io_download() {
                 Ok(ok) => ok,
                 Err(err) => return Err(format!("failed to download: {}", err))
