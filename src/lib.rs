c827761c5dc6e455c2cd2e3ec4e5ce3ba19ed6b2
leviport <levi@system76.com>
2023-02-16 13:00:14 -0700

    Add thelio-major-r4

--- src/lib.rs
+++ src/lib.rs
@@ -109,6 +109,7 @@ const MODEL_WHITELIST: &[&str] = &[
     "thelio-major-r2",
     "thelio-major-r2.1",
     "thelio-major-r3",
+    "thelio-major-r4",
     "thelio-mega-b1",
     "thelio-mega-r1",
     "thelio-mega-r1.1",
