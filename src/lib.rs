pop-os/system76-firmware

commit 4613ff0badba92557c666ee6c2e1c0c1a6cd4fd8

Update edition, deps, toolchain
- Update edition to 2021
- Update toolchain to 1.70.0
- Update deps

Signed-off-by: Tim Crawford <tcrawford@system76.com>

src/lib.rs
@@ -343,7 +343,7 @@ pub fn schedule_firmware_id(digest: &str, efi_dir: &str, firmware_id: &str) -> R
 
     remove_dir(&updater_dir)?;
 
-    let updater_tmp = match tempdir::TempDir::new_in(efi_dir, "system76-firmware-update") {
+    let updater_tmp = match tempfile::TempDir::with_prefix_in(efi_dir, "system76-firmware-update") {
         Ok(ok) => ok,
         Err(err) => {
             return Err(format!("failed to create temporary directory: {}", err));

Cargo.toml
@@ -2,7 +2,7 @@
 name = "system76-firmware"
 version = "1.0.54"
 authors = ["Jeremy Soller <jeremy@system76.com>"]
-edition = "2018"
+edition = "2021"
 
 [workspace]
 members = [ "daemon" ]
@@ -12,20 +12,20 @@ name = "system76-firmware-cli"
 path = "src/bin/cli.rs"
 
 [dependencies]
-buildchain = "0.4"
+anyhow = "1.0"
+bincode = "1.3"
+buildchain = "0.5"
 clap = { version = "3", features = ["derive"] }
 ecflash = { git = "https://github.com/system76/ecflash.git", branch = "stable" }
 libc = "0.2"
 plain = "0.2"
-rust-lzma = "0.5"
-serde = { version = "1.0.116", features = ["derive"] }
+rust-lzma = "0.6"
+serde = { version = "1.0", features = ["derive"] }
 serde_json = "1.0"
-sha2 = "0.9"
+sha2 = "0.10"
 tar = "=0.4.35"
-tempdir = "0.3"
-uuid = "0.8"
-bincode = "1.3"
-anyhow = "1.0"
+tempfile = "3.8"
+uuid = "1.4"
 
 [dependencies.system76_ectool]
 version = "0.2.1"

rust-toolchain
@@ -1,1 +1,0 @@
-1.61.0

rust-toolchain.toml
@@ -1,0 +1,3 @@
+[toolchain]
+channel = "1.70.0"
+components = ["clippy", "rustfmt"]

commit d94772321dee4f482881eef5e24a2d54bd29b10a

Fix argument order for `with_prefix_in`
The argument order is wrong, causing upgrades to fail when moving the
temp extract to the EFI partition.

    system76-firmware: failed to schedule: failed to move /boot/efiMG6RBk to /efi/boot/system76-firmware-update: Invalid cross-device link (os error 18)

Additionally, add a `.` as a separator to the prefix to exactly match
the old tempdir behavior, as tempfile does not add a separator itself.

Fixes: 4613ff0badba ("Update edition, deps, toolchain")
Signed-off-by: Tim Crawford <tcrawford@system76.com>

src/lib.rs
@@ -360,7 +360,7 @@ pub fn schedule_firmware_id(digest: &str, efi_dir: &str, firmware_id: &str) -> R
 
     remove_dir(&updater_dir)?;
 
-    let updater_tmp = match tempfile::TempDir::with_prefix_in(efi_dir, "system76-firmware-update") {
+    let updater_tmp = match tempfile::TempDir::with_prefix_in("system76-firmware-update.", efi_dir) {
         Ok(ok) => ok,
         Err(err) => {
             return Err(format!("failed to create temporary directory: {}", err));