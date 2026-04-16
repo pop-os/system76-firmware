6ccf11f17c0b055736c8e1701f5650be2119d69f
Jack Kelly <jpk.lly@gmail.com>
2026-04-16 12:00:15 -0400

    add flake and .envrc

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      fenix,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            fenix.packages.${system}.complete.toolchain
+          ];
+        };
+      }
+    );
+}
